72e5761b7e745e1f0d6103fd5b86ec889429d575
Alkrenawizid <z.i.d@msn.com>
2021-10-10 11:49:38 +0300

    First JS Exercise

---
+++ week2/day1/ex1/script.js
@@ -0,0 +1,6 @@
+let addressNumber = 125;
+let addressStreet = 'Ali Ben Abi Taleb';
+let country = 'Israel';
+
+let globalAddress = 'I Live at ' + addressNumber + ' ' + addressStreet + ', in ' + country;
+console.log(globalAddress);
