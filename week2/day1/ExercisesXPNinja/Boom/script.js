3c2bd9fe759671b2c393e0e8488435750ff40626
Alkrenawizid <z.i.d@msn.com>
2021-10-12 01:54:13 +0300

    Boom

--- week2/day1/ExercisesXPNinja/Boom/script.js
+++ week2/day1/ExercisesXPNinja/Boom/script.js
@@ -2,7 +2,16 @@ let number = prompt("Enter a number:");
 
 if (number < 2){
     alert('Boom')
-} else if (number > 2){
-    n = number.length;
-    alert('B'+ n + 'm')
+} else if (number > 2 & number % 2 !== 0 & number % 5 !== 0){
+    number = "o".repeat(number);
+    alert('B'+ number + 'm')
+} else if (number % 2 === 0 & number % 5 !== 0) {
+    number = "o".repeat(number);
+    alert('B'+ number + 'm!')
+} else if (number % 5 === 0) {
+    number = "O".repeat(number);
+    alert('B'+ number + 'M!')
+} else if (number % 2 === 0 & number % 5 === 0) {
+    number = "o".repeat(number);
+    alert('B'+ number + 'M!')
 }
