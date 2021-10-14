3dc6fbfed84c91183ca598e589e7b75dedc8563e
Alkrenawizid <z.i.d@msn.com>
2021-10-14 14:57:38 +0300

    Play The Game

--- week2/day5/PlayTheGame/script.js
+++ week2/day5/PlayTheGame/script.js
@@ -4,13 +4,14 @@ function playTheGame() {
     if (!play) {
         alert('No problem, Goodbye.')
     } else {
-        let number = prompt('Enter a number between 0 and 10')
+        let number = +prompt('Enter a number between 0 and 10')
         if (number != null & number != '') {
             if (!isNaN(number)) {
                 if (number <= 10 & number > 0) {
                     //alert (number + ' is a number')
                     let computerNumber = Math.floor(Math.random() * 11);
                     alert('The computer number is: ' + parseInt(computerNumber))
+                    test(number, computerNumber)
                 } else if (number > 10) {
                     alert('Sorry it’s not a good number, Goodbye')
                 }
@@ -22,15 +23,24 @@ function playTheGame() {
 }
 
 function test(userNumber, computerNumber) {
-    if (userNumber === computerNumber) {
-        alert('Winner!')
-    } else if (userNumber >= computerNumber) {
+    let i = 1;
+    do {
 
-        prompt('Your number is bigger then the computer’s, guess again')
+        if (userNumber >= computerNumber) {
 
-    } else if (userNumber <= computerNumber) {
+            userNumber = +prompt('Your number is bigger then the computer’s, guess again')
+            i++;
+        } else if (userNumber <= computerNumber) {
 
-        prompt('Your number is smaller then the computer’s, guess again')
+            userNumber = +prompt('Your number is smaller then the computer’s, guess again')
+            i++;
+        }
 
-    }
+        if (userNumber === computerNumber) {
+            alert('Winner!')
+        }
+        if (i === 4) {
+            break;
+        }
+    } while (userNumber !== computerNumber);
 }
