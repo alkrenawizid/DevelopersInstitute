12bdfdb236ef8db9b6912edaaa152dcdee3de82b
Alkrenawizid <z.i.d@msn.com>
2021-10-15 16:43:00 +0300

    Words In The Stars

--- week2/day4/DailyChallenge/WordsInTheStars/script.js
+++ week2/day4/DailyChallenge/WordsInTheStars/script.js
@@ -1,5 +1,18 @@
 //Prompt the user for several words (separated by commas).
 
-function Words() {
-    return prompt('Please enter words (separated by commas)').split(',');
+let str = prompt('Please enter words (separated by commas)').split(',');
+let length = 0;
+
+for (let index = 0; index < str.length; index++) {
+    if (length < str[index].length) {
+        length = str[index].length;       
+    }
 }
+
+console.log('*'.repeat(length+4));
+
+for (let index = 0; index < str.length; index++) {
+    console.log("* " + str[index] + " ".repeat(length + 1 - str[index].length) + "*");
+}
+
+console.log('*'.repeat(length+4));
