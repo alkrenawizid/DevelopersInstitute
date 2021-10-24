95310a13d7e241bbdf3196b265463f8e39ab9186
Alkrenawizid <z.i.d@msn.com>
2021-10-25 01:47:12 +0300

    Ternary Operator

---
+++ week4/day1/ExerciseXP/TernaryOperator/script.js
@@ -0,0 +1,19 @@
+//Using the code below :
+function winBattle(){
+    return true;
+}
+
+// function experiencePoints() {
+//     if (winBattle()) {
+//         return 10;
+//     } else {
+//         return 1;
+//     }
+// }
+
+//Create a variable called experiencePoints.
+//Assign to this variable, a ternary operator (ie. change the conditional block into a ternary operator).
+
+let experiencePoints = winBattle() ? 10 : 1;
+
+
