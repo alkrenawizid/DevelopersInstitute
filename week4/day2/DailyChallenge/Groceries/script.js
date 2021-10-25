fd7ab038941035c32303de9fee8b0258acdb783b
Alkrenawizid <z.i.d@msn.com>
2021-10-25 14:53:15 +0300

    Groceries

---
+++ week4/day2/DailyChallenge/Groceries/script.js
@@ -0,0 +1,15 @@
+// Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
+
+// Copy this object using the method that was taught in today’s lesson.
+// Change the value of totalPrice to 35$. Will we also see this modification in the copied objects ?
+// Change the value of payed to false. Will we also see this modification in the copied objects ? Why ?
+
+let groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
