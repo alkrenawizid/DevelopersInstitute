f8e6f896be42eb26ac3d2002db9aa16f15a6c7cb
Alkrenawizid <z.i.d@msn.com>
2021-10-31 16:24:14 +0200

    JSON Mario

---
+++ week5/day1/ExercisesXP/Mario/script.js
@@ -0,0 +1,32 @@
+let marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+// const toObj = JSON.parse(marioGame);
+// console.log(toObj)
+
+const toJson = JSON.stringify(marioGame)
+console.log(toJson)
+
+const toJsonPretty = JSON.stringify(marioGame, null, 2)
+console.log(toJsonPretty) 
