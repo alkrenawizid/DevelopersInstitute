b73f5279b6da73f765da9874c57887786e8f6e49
Alkrenawizid <z.i.d@msn.com>
2021-11-03 01:11:04 +0200

    xercise XP Gold

---
+++ week5/day3/ExerciseXPGold/script.js
@@ -0,0 +1,16 @@
+
+const urls = [
+    'https://swapi.dev/api/people/1',
+    'https://swapi.dev/api/people/2',
+    'https://swapi.dev/api/people/3',
+    'https://swapi.dev/api/people/4'
+  ]
+
+// map every url to the promise of the fetch
+let requests = urls.map(url => fetch(url));
+
+// Promise.all waits until all jobs are resolved
+Promise.all(requests)
+  .then(responses => responses.forEach(
+    response => console.log(response)
+  ));
