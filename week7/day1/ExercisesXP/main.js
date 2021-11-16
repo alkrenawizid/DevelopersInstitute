685290cd8917abc4370d7e1f8e0d9531d6571203
Alkrenawizid <z.i.d@msn.com>
2021-11-17 01:35:11 +0200

    Exercises XP

---
+++ week7/day1/ExercisesXP/main.js
@@ -0,0 +1,20 @@
+// 1
+// let a = 5;
+// let b = 10;
+// let c = a + b
+
+// console.log(`Hello you are ${c}`)
+
+// 2
+console.log('Hi, thanks for running me with npm start!');
+
+// 3
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+
+console.log(`First Run ${x}`)
+
+console.log(`Second Run ${x+c}`)
