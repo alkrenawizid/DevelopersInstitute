74bb91a172abd9c1880f66983fa0bcda7550cdc8
Alkrenawizid <z.i.d@msn.com>
2021-11-22 20:15:54 +0200

    Countries API

---
+++ week8/day2/countries-api/modules/db.js
@@ -0,0 +1,20 @@
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection:{
+        host: '127.0.0.1',
+        port: '5432',
+        user: 'postgres',
+        password: 'root',
+        database: 'countries'
+    }
+})
+
+const getContries = () => {
+    return db('country').select('country_id','country').orderBy('country')
+}
+
+module.exports = {
+    getContries
+}
