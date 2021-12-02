aa3ecb868ec928966de3ee7ecfde69d8953b937b
Alkrenawizid <z.i.d@msn.com>
2021-12-02 20:38:04 +0200

    React Carousel

---
+++ week8/day1/react-carousel/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ReactCarousel from './components/ReactCarousel'
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ height: "400px", width: "400px", margin: "auto" }}>
+        <ReactCarousel />
+      </div>
+    </div>
+  );
+}
+
+export default App;
